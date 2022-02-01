a1cd9e3d36c40c9979b66b27052833110f61d4d3
Selvamvirtuo <mailtomariiselvam@gmail.com>
2022-02-01 20:34:37 +0530

    typescript examples

---
+++ feb-01/test.js
@@ -0,0 +1,9 @@
+var message = "Testing for ts";
+console.log(message);
+var rt = "Testing for rt";
+console.log(rt);
+function addnum(a, b) {
+    return a + b;
+}
+var sum = addnum(15, 25);
+console.log(sum);
