60044ed94c42a05e0f492d0eb713e8014d2c15c3
Selvamvirtuo <mailtomariiselvam@gmail.com>
2022-02-11 20:00:56 +0530

    External js file add

---
+++ feb-11/addition.js
@@ -0,0 +1,38 @@
+export const a = [1,2,3,4,5];
+export const str = "Export & Import Module";
+
+// Default function 
+export default function (a,b) {
+  return a * b;
+}
+
+// Named function 
+export function names(c,d){
+  return c + d;
+}
+
+// Class 
+export class User {
+  
+  constructor(name,email){
+    this.uname = name;
+    this.mail = email;
+  }
+  courseList = [];
+  getInfo(){
+    return { name:this.uname , email : this.mail };
+  }
+
+  addCourse(name){
+    this.courseList.push(name);
+  }
+
+  getCourse(){
+    return this.courseList;
+  }
+}
+
+const getFullname = (name, sname) =>{
+  return name + ' ' + sname;
+};
+module.export = getFullname;
