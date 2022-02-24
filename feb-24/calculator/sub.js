348daa8e0c8bbe49588af8e022e6fdac1ad4cbdc
Selvamvirtuo <mailtomariiselvam@gmail.com>
2022-02-24 19:06:15 +0530

    Create multiple folder to check in AMD run

---
+++ feb-24/calculator/sub.js
@@ -0,0 +1,12 @@
+// var calcluator = calcluator || {};
+
+// calcluator.sub = function(a,b){
+//   return a - b;
+// };
+
+
+define(function(){
+  return function(a,b){
+    return a - b;
+  };
+});
